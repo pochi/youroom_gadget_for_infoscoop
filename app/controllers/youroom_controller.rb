e06bb4f22aa5e24bb8fd50f89a24670bd360f830
pochi <pochi.black@gmail.com>
2010-06-07 18:33:17 +0900

    Get room_list using oauth

--- app/controllers/youroom_controller.rb
+++ app/controllers/youroom_controller.rb
@@ -1,5 +1,28 @@
-class YouroomController < ApplicationController
+require 'youroom_api'
 
-  def list
+class YouroomController < ApplicationController
+  def index
   end
+
+  def list_html
+    user_id = request.headers["HTTP_IS_USER"]
+
+    consumer = OAuth::Consumer.new("up3xopytN53tYzIiy4v7",
+                                   "oii4Nl9qEOhumFji6Uq6In5VU4VbhK697hV078kJ",
+                                   :site => "https://api.youroom.in/")
+
+    info = consumer.get_access_token(nil, { },
+                                     {:x_auth_mode=>"client_auth",
+                                       :x_auth_username=>"pochi.black@gmail.com",
+                                       :x_auth_password=>"eyefunny"})
+
+    access_token = OAuth::AccessToken.new(consumer,
+                                          info.params['oauth_token'],
+                                          info.params['oauth_token_secret'])
+
+    client = Youroom::Request.new(access_token)
+    logger.info client.get_my_group.body
+    render :json => client.get_my_group.body
+  end
+
 end
