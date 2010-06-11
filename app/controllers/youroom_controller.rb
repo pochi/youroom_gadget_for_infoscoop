f189e7e81845ccaf95f5ca8fd13516d788a3b6ec
pochi <pochi.black@gmail.com>
2010-06-11 11:53:27 +0900

    Authenticate with infoscoop authentication

--- app/controllers/youroom_controller.rb
+++ app/controllers/youroom_controller.rb
@@ -1,26 +1,30 @@
 class YouroomController < ApplicationController
+  before_filter :setup_user
+
   def index
   end
 
   def list_html
-    user_id = request.headers["HTTP_IS_USER"]
-
-    consumer = OAuth::Consumer.new("up3xopytN53tYzIiy4v7",
-                                   "oii4Nl9qEOhumFji6Uq6In5VU4VbhK697hV078kJ",
-                                   :site => "https://api.youroom.in/")
-
-    info = consumer.get_access_token(nil, { },
-                                     {:x_auth_mode=>"client_auth",
-                                       :x_auth_username=>"pochi.black@gmail.com",
-                                       :x_auth_password=>"eyefunny"})
-
-    access_token = OAuth::AccessToken.new(consumer,
-                                          info.params['oauth_token'],
-                                          info.params['oauth_token_secret'])
-
     client = Youroom::Request.new(access_token)
     logger.info client.get_my_group.body
     render :json => client.get_my_group.body
+  end
+
+  private
+  def setup_user
+    @user = User.find_by_bpr(request.headers["HTTP_IS_USER"])
+  end
+
+  def consumer
+    @consumer ||= ::OAuth::Consumer.new(@user.youroom_oauth.consumer_key,
+                                        @user.youroom_oauth.consumer_secret,
+                                        :site=>"https://api.youroom.in/")
+  end
+
+  def access_token
+    @access_token ||= ::OAuth::AccessToken.new(consumer,
+                                               @user.youroom_oauth.access_token,
+                                               @user.youroom_oauth.access_token_secret)
   end
 
 end
