5c37d84b036cc05b9524f5201a18be900ffac59d
pochi <pochi.black@gmail.com>
2010-06-11 16:22:58 +0900

    Modified show

--- app/controllers/youroom_controller.rb
+++ app/controllers/youroom_controller.rb
@@ -1,22 +1,28 @@
 class YouroomController < ApplicationController
-  before_filter :setup_user, :only=>[:show, :list_html]
+  before_filter :setup_user, :only=>[:list_html, :entries]
 
   def index
   end
 
   def show
-    logger.info params.inspect
+    @room_id = params[:id]
   end
 
   def list_html
-    client = Youroom::Request.new(access_token)
-    logger.info client.get_my_group.body
     render :json => client.get_my_group.body
+  end
+
+  def entries
+    render :json => client.get_entry(params[:id]).body
   end
 
   private
   def setup_user
     @user = User.find_by_bpr(request.headers["HTTP_IS_USER"])
+  end
+
+  def client
+    @client ||= Youroom::Request.new(access_token)
   end
 
   def consumer
